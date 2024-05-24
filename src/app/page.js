be2f98de72c403e32af845caa2324e26b400f44d
Oğuzcan Toptaş <oguzcantoptas@gmail.com>
2024-05-24 20:08:10 +0300

    make cookies great again

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,103 @@
 'use client'
 
 import Vendor from '@/components/Vendor'
+import Cookies from 'js-cookie'
 
 import { useState, useEffect } from 'react'
 
 const Home = () => {
-  const [money, setMoney] = useState(50)
+  const savedMoney = Cookies.get('money')
+  const savedBuildings = Cookies.get('buildings')
 
-  const [buildings, setBuildings] = useState([
-    {
-      id: 0,
-      name: 'Simitçi',
-      cost: 50,
-      number: 0,
-      income: 10,
-      duration: 1000,
-    },
-    {
-      id: 1,
-      name: 'Tostçu',
-      cost: 150,
-      number: 0,
-      income: 30,
-      duration: 2000,
-    },
-    {
-      id: 2,
-      name: 'Cafe',
-      cost: 300,
-      number: 0,
-      income: 100,
-      duration: 3000,
-    },
-  ])
+  const [money, setMoney] = useState(savedMoney || 50)
+
+  const [buildings, setBuildings] = useState(
+    JSON.parse(savedBuildings) || [
+      {
+        id: 0,
+        name: 'Simitçi',
+        cost: 50,
+        number: 0,
+        income: 10,
+        duration: 1000,
+      },
+      {
+        id: 1,
+        name: 'Tostçu',
+        cost: 150,
+        number: 0,
+        income: 30,
+        duration: 2000,
+      },
+      {
+        id: 2,
+        name: 'Cafe',
+        cost: 300,
+        number: 0,
+        income: 100,
+        duration: 3000,
+      },
+    ]
+  )
+
+  useEffect(() => {
+    const intervals = buildings.map((building) => {
+      return setInterval(() => {
+        setMoney((prevMoney) => prevMoney + building.number * building.income)
+      }, building.duration)
+    })
+
+    return () => intervals.forEach((interval) => clearInterval(interval))
+  }, [buildings])
+
+  useEffect(() => {
+    const saveData = async () => {
+      try {
+        await Promise.all([
+          Cookies.set('money', money, { expires: 2, sameSite: 'Strict' }),
+          Cookies.set('buildings', JSON.stringify(buildings), {
+            expires: 2,
+            sameSite: 'Strict',
+          }),
+        ])
+
+        console.log('saved data: ' + money + buildings)
+      } catch (error) {
+        console.error('Error saving data to cookies:', error)
+      }
+    }
+
+    saveData()
+
+    const saveInterval = setInterval(saveData, 10000)
+
+    return () => clearInterval(saveInterval)
+  }, [money, buildings])
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const savedMoney = Cookies.get('money')
+        const savedBuildings = Cookies.get('buildings')
+
+        if (savedMoney) {
+          setMoney(Number(savedMoney))
+        } else {
+          throw new Error('Error fetching money')
+        }
+
+        if (savedBuildings) {
+          setBuildings(JSON.parse(savedBuildings))
+        } else {
+          throw new Error('Error fetching buildings')
+        }
+      } catch (error) {
+        console.log('Error loading save: ', error)
+      }
+    }
+
+    loadData()
+  }, [])
 
   function handlePurchase(buildingId) {
     const updatedBuildings = buildings.map((building) => {
@@ -49,34 +114,32 @@ const Home = () => {
     setBuildings(updatedBuildings)
   }
 
-  useEffect(() => {
-    const intervals = buildings.map((building) => {
-      return setInterval(() => {
-        setMoney((prevMoney) => prevMoney + building.number * building.income)
-      }, building.duration)
-    })
-
-    return () => intervals.forEach((interval) => clearInterval(interval))
-  }, [buildings])
-
-  // useEffect(() => {
-  //   console.log(money)
-  // }, [money])
+  function resetCookies() {
+    Cookies.remove('money')
+    Cookies.remove('buildings')
+    console.log('Cookies removed')
+  }
 
   return (
     <div className="text-center">
-      <h1>Your money: {money}$</h1>
-      <h2>Your buildings</h2>
+      <div>
+        <h1>Your money: {money}$</h1>
+        <button onClick={() => resetCookies()} className="bg-slate-200">
+          Reset Cookies
+        </button>
+      </div>
       <hr />
-      {buildings.map((building) => {
-        return (
-          <Vendor
-            key={building.id}
-            building={building}
-            handlePurchase={handlePurchase}
-          />
-        )
-      })}
+      <h2>Your buildings</h2>
+      {buildings &&
+        buildings.map((building) => {
+          return (
+            <Vendor
+              key={building.id}
+              building={building}
+              handlePurchase={handlePurchase}
+            />
+          )
+        })}
     </div>
   )
 }

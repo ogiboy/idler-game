3f3867aca0ae8c3aaf02dd46db683117e7ec1710
Oğuzcan Toptaş <oguzcantoptas@gmail.com>
2024-05-25 23:00:39 +0300

    add autosave message

--- src/app/page.js
+++ src/app/page.js
@@ -211,6 +211,9 @@ const Home = () => {
                 </animated.div>
               )
           )}
+          <p className="fixed bottom-0 w-screen text-center bg-blue-300/50">
+            This game uses an autosave feature 💾
+          </p>
         </div>
       ) : (
         <h1>Loading...</h1>
